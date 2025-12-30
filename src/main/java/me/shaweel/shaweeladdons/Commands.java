c6ab40f27104a45ef54d2e9b0866df2ac10b7e4b
shaweel <shaweel@proton.me>
2025-12-30 14:34:31 +0100

    Expanded the file structure

---
+++ src/main/java/me/shaweel/shaweeladdons/Commands.java
@@ -0,0 +1,5 @@
+package me.shaweel.shaweeladdons;
+
+public class Commands {
+        
+}
---
+++ src/main/java/me/shaweel/shaweeladdons/ConfigManager/ConfigManager.java
@@ -0,0 +1,5 @@
+package me.shaweel.shaweeladdons.ConfigManager;
+
+public class ConfigManager {
+        
+}
